22c8e41c55e2d8e00ccc7ed2590e1ef9b2b57d5f
mattvioli <mattvioli@gmail.com>
2023-02-21 18:16:06 +0700

    updated build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,21 +3,21 @@ type FormatType = {
     code: FormatLocale;
     options?: Intl.NumberFormatOptions;
 };
-export default class Decimal {
+export default class BIP {
     #private;
     static FORMAT_EUR: FormatType;
     static FORMAT_DOLLAR: FormatType;
     static FORMAT_TOKEN: FormatType;
     static FORMAT_PERCENT: FormatType;
     constructor(value: bigint, exponent?: bigint);
-    static from(value: bigint, exponent?: bigint): Decimal;
+    static from(value: bigint, exponent?: bigint): BIP;
     unscale(precision: bigint): bigint;
     toNumber(): number;
     toString(): string;
     toFormat(locale: FormatType, fractionalLength?: number): string;
-    add(other: Decimal): Decimal;
-    sub(other: Decimal): Decimal;
-    mul(other: Decimal): Decimal;
-    div(other: Decimal): Decimal;
+    add(other: BIP): BIP;
+    sub(other: BIP): BIP;
+    mul(other: BIP): BIP;
+    div(other: BIP): BIP;
 }
 export {};
